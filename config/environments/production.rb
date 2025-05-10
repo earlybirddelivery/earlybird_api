6340bedaf7436bbfa06c9ecbe3eebb2003abf445
earlybirddelivery <earlybirddeliveryservices@gmail.com>
2025-05-10 10:49:37 +0530

    add domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  config.hosts << "api.earlybirddelivery.in"
+
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
